6d7e3ffa802068228b7c945be1686311246a8729
Zhihao <zhihao.duch@gmail.com>
2022-06-26 20:17:06 +0200

    add fixes

--- server/src/models/models.py
+++ server/src/models/models.py
@@ -1,7 +1,10 @@
 from src import db
 
+
 class UserExperiment(db.Model):
     __tablename__ = 'USER_EXPERIMENTS'
+    __table_args__ = {'extend_existing': True}
+
     user_id = db.Column(db.Integer, db.ForeignKey("USERS.id"))
     experiment_id = db.Column(db.String(32), primary_key=True)
     user = db.relationship('User')
@@ -9,6 +12,8 @@ class UserExperiment(db.Model):
 
 class User(db.Model):
  __tablename__ = 'USERS'
+ __table_args__ = {'extend_existing': True}
+
  id = db.Column(db.Integer, autoincrement=True, primary_key=True)
  username = db.Column(db.String(25), nullable=False)
  email = db.Column(db.String(50), nullable=False)
