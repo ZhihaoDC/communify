79fa16ccf78b9ab218e9be03e3c1f50b875572b8
Zhihao <zhihao.duch@gmail.com>
2022-10-09 03:14:25 +0200

    init db from backend

--- server/src/__init__.py
+++ server/src/__init__.py
@@ -1,12 +1,18 @@
-from . import app_config
+
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
+
+from sqlalchemy.exc import OperationalError
+import time
+
+from . import app_config
 from src.plugins.SQLAlchemy import db
+from src.api.ExperimentsController import ExperimentsController
 from src.api.LouvainController import LouvainController
 from src.api.GirvanNewmanController import GirvanNewmanController
 from src.api.GraphVisualizationController import GraphVisualizationController
-from src.api.ExperimentsController import ExperimentsController
+from src.models.UserModel import User
 ############################################## APP #################################################
 
 
@@ -27,14 +33,42 @@ def create_app():
 
     # Start app and database
     db.init_app(app)
-    # db.create_all()
-    
-    migrate = Migrate(app, db)
+
+    #Retry on connection failure
+    MAX_RETRIES = 10
+    TIME_RETRY_SECS = 5
+    retry_count = MAX_RETRIES
+    is_db_connected = False
+    while retry_count >= 0 and not is_db_connected:
+        try:
+            db.drop_all()
+            db.create_all()
+            is_db_connected = True
+        except OperationalError:
+            print(f"Attempting to reconnect to database... (Retry again in {TIME_RETRY_SECS} seconds...)")
+            time.sleep(TIME_RETRY_SECS)
+            if retry_count == 0:
+                print(f"Connection failed. (Tried to reconnect {MAX_RETRIES} times")
+
+    # migrate = Migrate(app, db)
+    init_database()
 
     # enable CORS
     CORS(app, resources={r'/*': {'origins': '*'}})
 
     return app
+
+
+def init_database():
+    default_user = User(username="david19",
+        email="david19@gmail.com", 
+        firstname="david", 
+        lastname="fernandez", 
+        profile_description="data scientist")
+        
+    db.session.add(default_user)
+
+    db.session.commit()
 
 
 if __name__ == '__main__':
