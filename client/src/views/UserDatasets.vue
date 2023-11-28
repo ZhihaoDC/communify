a3d13671b55db25c0f2cd4cd4bc9ce4dddcce344
Zhihao <zhihao.duch@gmail.com>
2023-11-28 01:36:48 +0100

    add modal to each row of dataset table

--- client/src/views/UserDatasets.vue
+++ client/src/views/UserDatasets.vue
@@ -1,21 +1,31 @@
 
 <template>
     <b-container fluid="md">
-        <div>
+      <div>
         <h1 id="header">Datasets guardados</h1>
         <h4>
         Datasets guardados por el usuario
         </h4>
         
-        
-    
+      
         <b-table
             id="my-table"
             :items="datasets"
+            :fields="fields"
             :per-page="perPage"
             :current-page="currentPage"
             small
-        ></b-table>
+        >
+        
+        <template #cell(actions)="row">
+          <b-button size="sm" @click="info(row.item, row.index, $event.target)" class="mr-1">
+            Info modal
+          </b-button>
+        </template>
+        </b-table>
+        <b-modal :id="infoModal.id" :title="infoModal.title" ok-only @hide="resetInfoModal">
+          <pre>{{ infoModal.content }}</pre>
+        </b-modal>
         <div>
         <b-pagination
             v-model="currentPage"
@@ -24,7 +34,7 @@
             aria-controls="my-table"
         ></b-pagination>
         </div>
-        </div>
+      </div>
     </b-container>
 </template>
   
@@ -34,7 +44,18 @@
         return {
           perPage: 3,
           currentPage: 1,
-          datasets: []
+          datasets: [],
+          fields: [
+            {key: 'name', label: 'nombre dataset'},
+            {key: 'creation_date', label: 'fecha creacion'},
+            {key: 'nodes', label: 'nodos'},
+            {key: 'actions', label: 'acciones'}
+          ],
+          infoModal: {
+            id: 'info-modal',
+            title: '',
+            content: ''
+          }
         }
       },
       computed: {
@@ -66,9 +87,10 @@
           format_dataset(dataset){
             let dataset_nodes = dataset.json.elements.nodes.map(node => node.data.name)
             let dataset_json = {
-              nombre : dataset['name'],
-              fecha_creacion: dataset['creation_date'],
-              nodos : dataset_nodes.join(', ').substring(0, 200) + " ..."
+              name : dataset['name'],
+              id: dataset['id'],
+              creation_date: dataset['creation_date'],
+              nodes : dataset_nodes.join(', ').substring(0, 200) + " ..."
             }
             return dataset_json
           },
@@ -76,7 +98,13 @@
           format_datasets(datasets){
              let formatted_dataset = datasets.map(dataset => this.format_dataset(dataset))
             return formatted_dataset
-          }
+          },
+          info(item, index, button) {
+            this.infoModal.title ='Row index: ${index}'
+            this.infoModal.content = JSON.stringify(item, null, 2)
+            this.$root.$emit('bv::show::modal', this.infoModal.id, button)
+            console.log(index)
+          },
         }
       
     }
